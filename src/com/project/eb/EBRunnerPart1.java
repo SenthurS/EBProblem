f9e28edf8fd1a5baf8761bf3382fd8668bab0371
senthur <senthurshanmugasundaram@gmail.com>
2022-12-15 13:22:15 +0530

    commit from gui

--- src/com/project/eb/EBRunnerPart1.java
+++ src/com/project/eb/EBRunnerPart1.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Scanner;
-
+ 
 public class EBRunnerPart1 {
 
 	public static void main(String[] args) {
